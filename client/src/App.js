c6a4f4cf61d12269b2a61b4b09c1aaa24382263e
IamMonkie <monkie_225@yahoo.com>
2019-05-08 00:26:37 -0400

    bootstrap installed

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,30 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 
-// import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-
-        <p />
-      </header>
-      <body className="App-body">
-        <h1>APP</h1>
-      </body>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+        <body className="App-body">
+          {/* <Router>
+            <Switch>
+              <Route exact path="/" Component={Home} />
+              <Route path="/about" Component={About} />
+              <Route path="/contact" Component={Contact} />
+              <Route component={noMatch} />
+            </Switch>
+          </Router> */}
+          <h1>APP</h1>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
