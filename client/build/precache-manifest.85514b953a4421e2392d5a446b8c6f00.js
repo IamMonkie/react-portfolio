117d1982c351c025d9329f76c972c593cce18b88
IamMonkie <monkie_225@yahoo.com>
2019-05-11 18:00:15 -0400

    build created

---
+++ client/build/precache-manifest.85514b953a4421e2392d5a446b8c6f00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7059f7b5e9ee7287c9a034af84001406",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88c4e1a8edf18e647d57",
+    "url": "/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "8c8b7419dbfca6fa77b8",
+    "url": "/static/js/2.6b58cc55.chunk.js"
+  },
+  {
+    "revision": "88c4e1a8edf18e647d57",
+    "url": "/static/js/main.ab3c0770.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "352baa1d633df4e2ce6f8ac645373c30",
+    "url": "/static/media/photo_aboutMe.352baa1d.jpg"
+  }
+]);
