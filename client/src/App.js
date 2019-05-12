0cd726dd948da14c5a69dd1ca81dade379c7925d
IamMonkie <monkie_225@yahoo.com>
2019-05-12 13:22:56 -0400

    carousel added

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { Container } from "react-bootstrap";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { MDBAnimation } from "mdbreact";
+import styles from "react-responsive-carousel/lib/styles/carousel.min.css";
 
 // import "bootstrap/dist/css/bootstrap.min.css";
 import MainNavbar from "./components/Navbar";
 import ContactForm from "./components/ContactForm";
 
 import AboutMe from "./pages/AboutMe";
+import Applications from "./pages/applications";
 import Resume from "./pages/resume";
 
 class App extends Component {
@@ -24,10 +26,13 @@ class App extends Component {
               <AboutMe />
             </MDBAnimation>
             <MDBAnimation type="fadeInUpBig" delay="1.25s">
+              <Applications />
+            </MDBAnimation>
+            <MDBAnimation type="fadeInUpBig" delay="1.5s">
               <Resume />
             </MDBAnimation>
 
-            <MDBAnimation type="fadeInUpBig" delay="1.5s">
+            <MDBAnimation type="fadeInUpBig" delay="1.75s">
               <ContactForm />
             </MDBAnimation>
 
